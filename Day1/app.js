d9203f8a19d46608a26df4b7def746bb16f9e2c1
SATYAM MAURYA <satyammaurya24363@gmail.com>
2026-04-15 00:14:47 +0530

    DAY1 BASIC

---
+++ Day1/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 10;
+let b = 10;
+let c = "satyam";
+console.log(a * 10);
+console.log(c + 10);
+let isTrue = true;
+const d = NaN;
+//# sourceMappingURL=app.js.map
---
+++ Day1/app.ts
@@ -0,0 +1,7 @@
+let a:number=10;
+let b:number=10;
+let c:string="satyam";
+console.log(a*10);
+console.log(c+10);
+let isTrue:boolean=true;
+const d=NaN;

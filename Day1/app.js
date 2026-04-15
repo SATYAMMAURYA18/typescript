e11bc59447485ceaae29de9d27fd848d3a31cb12
SATYAM MAURYA <satyammaurya24363@gmail.com>
2026-04-15 21:00:27 +0530

    Object in typescript

--- Day1/app.js
+++ Day1/app.js
@@ -20,4 +20,35 @@ let tup = ['rohit', 67]; //tuple fix size ka hota hai
 //tuple me jitne datatype declair kroge utna hi us type ko daloge
 //for example 4 number dalne hai toh 4 bar number assign krana padega phir order bhi matter krta hai
 let tup2 = [65, 78, 'satyam', 84, true];
+//object
+let obj1 = {
+    name: "Satyam",
+    age: 20,
+    gender: "Male"
+};
+//we can initialize like that also
+let obj2 = {
+    name: "Satyam",
+    age: 20,
+    gender: "Male"
+};
+//we can initialize like that also
+let person;
+person = {
+    name: "Satyam",
+    age: 21,
+    balance: 344643466643
+};
+let c1 = {
+    name: "Patel",
+    age: 21,
+    balance: 654367,
+    sex: "Female"
+};
+let c2 = {
+    name: "Patel",
+    age: 21,
+    balance: 654367,
+    sex: "Female"
+};
 //# sourceMappingURL=app.js.map
--- Day1/app.ts
+++ Day1/app.ts
@@ -19,3 +19,53 @@ let tup: [string, number] = ['rohit', 67];//tuple fix size ka hota hai
 //tuple me jitne datatype declair kroge utna hi us type ko daloge
 //for example 4 number dalne hai toh 4 bar number assign krana padega phir order bhi matter krta hai
 let tup2: [number, number, string, number, boolean] = [65, 78, 'satyam', 84, true];
+
+
+//object
+let obj1={
+    name:"Satyam",
+    age:20,
+    gender:"Male"
+}
+//we can initialize like that also
+let obj2:{name:string,age:number,gender:string}={
+    name:"Satyam",
+    age:20,
+    gender:"Male"
+}
+//we can initialize like that also
+let person:{name:string,age:number,balance:number};
+person={
+   name:"Satyam",
+   age:21,
+   balance:344643466643
+}
+//we can initialize like that also
+type custommer={
+    name:string,
+    age:number,
+    balance:number,
+    sex:string
+}
+
+let c1:custommer={
+     name:"Patel",
+    age:21,
+    balance:654367,
+    sex:"Female"
+}
+
+//we use mostly interface rather than object in typescript
+interface admin{
+     name:string,
+    age:number,
+    balance:number,
+    sex:string
+}
+
+let c2:admin={
+     name:"Patel",
+    age:21,
+    balance:654367,
+    sex:"Female"
+}
